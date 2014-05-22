0a3e22098931400666fc8713dccf737a1c76b4aa
muzfuz <mazin.power@icloud.com>
2014-05-22 17:40:23 +0100

    feature complete

--- Sketchpad/script.js
+++ Sketchpad/script.js
@@ -1,29 +1,50 @@
-var input = 96;
-var boxSize = 960 / input-2;
-
-
 $(document).ready(function () {
 
-	//Fill up #grid with square <div> tags, with the .box class
-	for (var i = 0; i<(input*input); i++) {
+	//Initialise Gride
+	var input = 64;
+	FillGrid(input);
+	AddColor();
+
+	//Button is Clicked Function
+	$('button').click(function(){
+		$('.box').remove();
+		input = parseInt(prompt('Please enter a number between 1 and 128'));
+			if (input > 128) {
+				input = 128;
+			}
+			else if (input < 1) {
+				input = 1;
+			}
+
+		FillGrid(input);
+		AddColor();
+	});
+
+});
+
+//Function to create our grid. Square <div> items tagged with the .box class.
+function FillGrid(boxes){
+	for (var i = 0; i<(boxes*boxes); i++) {
 		$('<div/>', {
 			'class'	: 'box',
 		}).appendTo('#grid');
 	}
 
+	var boxSize = 960 / boxes;
 	//Adjust .box size
 	$('.box').css({
 		'width' : boxSize,
 		'height': boxSize
 	});
+}
 
-	//Hover Effect
-	$('.box').mouseenter(function(){
-		$(this).css('background-color', 'blue');
-
+//Change Colour Function
+function AddColor(){
+	$('.box').mouseenter(function(){	
+		$(this).addClass('box_coloured');
 		$(this).mouseleave(function(){
 			$(this).fadeTo('slow', 0.7);			
 		});
 	});
 
-});
+}
