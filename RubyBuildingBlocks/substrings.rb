1aa09afc01428374caf12e1786be51b3097e32d0
muzfuz <mazin.power@icloud.com>
2014-06-03 13:53:47 +0100

    substrings

--- RubyBuildingBlocks/substrings.rb
+++ RubyBuildingBlocks/substrings.rb
@@ -19,14 +19,3 @@ word_list = ["below","down","go","going","horn","how","howdy","it","i","low","ow
 word_string = "Howdy partner, sit down! How's it going?"
 
 substrings("word_string", word_list)
-
-
-# puts "Text please: "
-# text = gets.chomp
-
-# words = text.split(" ")
-# frequencies = Hash.new(0)
-# words.each { |word| frequencies[word] += 1 }
-# frequencies = frequencies.sort_by {|a, b| b }
-# frequencies.reverse!
-# frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
