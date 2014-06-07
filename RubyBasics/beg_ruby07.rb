8ce921131a18b8b57f596865c982918bd79b4ca8
muzfuz <mazin.power@icloud.com>
2014-06-07 16:19:20 +0100

    enumerable

--- RubyBasics/beg_ruby07.rb
+++ RubyBasics/beg_ruby07.rb
@@ -1,3 +1,12 @@
-require './string_extension'
-
-puts "This is a test".vowels.join('-')
+# This class stores information about people.
+class Person
+  attr_accessor :name, :age, :gender
+  # Create the person object and store their name
+  def initialize(name)
+    @name = name
+  end
+  # Print this person's name to the screen
+  def print_name
+    puts "Person called #{@name}"
+  end
+end
--- RubyBuildingBlocks/enumerable.rb
+++ RubyBuildingBlocks/enumerable.rb
@@ -20,5 +20,87 @@ module Enumerable
   	output
   end
 
+  def my_all?
+    if block_given?
+      self.my_each { |element| return false unless (yield(element) == true) }
+    else
+      self.my_each { |element| return false unless element == true}
+    end
+    true
+  end
+
+  def my_any?
+    if block_given?
+      self.my_each { |element| return true if (yield(element) == true) }
+    else
+      self.my_each { |element| return true if element == true}
+    end
+    false
+  end
+
+  def my_none?
+    if block_given?
+      self.my_each { |element| return false if (yield(element) == true) }
+    else
+      self.my_each { |element| return false if element == true}
+    end
+    true
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      self.my_each { |element| count +=1 if (yield(element) == true)}
+    else
+      self.my_each { |element| count += 1 }
+    end
+    count
+  end
+
+  def my_map
+    output = []
+    return self unless block_given?
+    self.my_each { |element| output << (yield(element))}
+    output
+  end
+
+  def my_inject(num=nil)
+    if num == nil
+      total = self.first
+    else
+      total = num
+    end
+
+    self.my_each do |element|
+      total = (yield(total, element))
+    end
+    total
+  end
+
+
 end
+
+puts [1, 2, 3, 4].inject(0) { |result, element| result + element }
+puts [1, 2, 3, 4].my_inject(0) { |result, element| result + element }
+
+longest = %w{ cat sheep bear }.my_inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+puts longest
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
