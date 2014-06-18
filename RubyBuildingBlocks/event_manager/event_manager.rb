d9bde97af40abc295ae779e87a30b920712e01e4
muzfuz <mazin.power@icloud.com>
2014-06-18 15:45:51 +0100

    complete

--- RubyBuildingBlocks/event_manager/event_manager.rb
+++ RubyBuildingBlocks/event_manager/event_manager.rb
@@ -38,20 +38,61 @@ def clean_phone(phone_number)
 	end		
 end
 
-puts "EventManager initialized."
+def peak_hours(hours_hash)	
+	max_registrations = hours_hash.select { |hour, amount| amount == hours_hash.values.max }
+	max_registrations.keys.join(', ')
+end
+
+def peak_days(days_hash)
+	max_registrations = days_hash.select { |day, amount| amount == days_hash.values.max }
+	max_registrations.keys.join(', ')
+end
+
+puts "EventManager initialized. List of registrants:"
+puts ""
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
+hours_hash = Hash.new(0)
+days_hash = Hash.new(0)
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   phone_number = clean_phone(row[:homephone])
   zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{phone_number}"
+  form_letter = erb_template.result(binding)
 
-  # save_thank_you_letters(id,form_letter)
+  
+  save_thank_you_letters(id,form_letter)
+  
+  reg_date = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M')
+  hours_hash[reg_date.strftime('%H')] += 1
+  days_hash[reg_date.strftime('%A')] += 1
+
+  puts "#{name} #{phone_number} #{zipcode}"
+
 end
+
+puts ""
+puts "Peak registration hour(s): #{peak_hours(hours_hash)}"
+puts "Peak registration day(s): #{peak_days(days_hash)}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
