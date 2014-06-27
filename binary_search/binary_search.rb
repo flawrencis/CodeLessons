4226af4b14d74d8d33718c40a2cf3f97939910bf
muzfuz <mazin.power@icloud.com>
2014-06-27 22:16:17 +0100

    depth first recursive complete

--- binary_search/binary_search.rb
+++ binary_search/binary_search.rb
@@ -1,3 +1,16 @@
+#Basic Binary Search Algorithm
+#BinarySearchTree class
+# 1. Class instance accepts an array as input
+# 2. Array is automatically sorted on initialize
+# 3. Tree is constructed from array items.
+# 4. Each item becomes an instance of the Node class
+# 5. Tree can now be searched using one of three search methods
+
+# 				SEARCH METHODS:
+# 				breadth_first_search(query)
+# 				depth_first_search(query)
+# 				dfs_recursive(query) 
+
 class Node
 	attr_accessor :value, :left_child, :right_child
 
@@ -33,7 +46,7 @@ class BinarySearchTree
 		@tree
 	end
 
-	def display_tree()						#Uses breadth first traversion to display the tree.
+	def display_tree()						#Use this function for debugging / displaying the tree.
 		list = []
 		yield @tree.value
 		left_child = @tree.left_child
@@ -79,21 +92,77 @@ class BinarySearchTree
 
 		loop do
 			return nil if stack.empty?
-			
+
 			node = stack.pop
 			return node if query == node.value
-			
+			p node.value
 			stack.push node.left_child if node.left_child != nil
 			stack.push node.right_child if node.right_child != nil
 		end
 	end
 
+	def dfs_recursive(query, node=@tree)		
+		return nil if node.nil?
+		return node if query == node.value
+
+		left_search = node.left_child != nil ? dfs_recursive(query, node.left_child) : nil
+		return left_search if left_search != nil
+		right_search = node.right_child != nil ? dfs_recursive(query, node.right_child) : nil
+		return right_search if right_search != nil 
+	end
+
+	def merge_sort(array)
+		len = array.length
+		return array if len == 1 				#Base case set to an array size of 1
+		
+		array1 = array[0..(len/2)-1]			#Split the array in half
+		array2 = array[len/2..-1]				#Array two gets the longer half if it's an odd length
+		a1 = merge_sort(array1)					#Split array1 in half again recursively
+		a2 = merge_sort(array2)					#Split array2 in half again recursively
+		merge(a1, a2)							#Call the merge function on the split arrays				
+	end
+
+	def merge(array1, array2, merged_array=[])
+		len_of_a1 = array1.length									#Get length of arrays and compare to the array's index
+		len_of_a2 = array2.length
+		index1 = 0													#Set the starting index for both arrays - always 0 since we are going left to right.
+		index2 = 0
+
+		while index1 < len_of_a1 and index2 < len_of_a2				#This loop continues until the end of one of the arrays is reached
+			if array1[index1] < array2[index2]						#It compares the index values of both arrays, and appends the one that is lower in value
+				merged_array << array1[index1]
+				index1 += 1
+			else
+				merged_array << array2[index2]
+				index2 += 1
+			end
+		end
+
+		if index1 < len_of_a1										#Appends any remaining values to the merged array
+			merged_array += array1[index1..-1]						
+		elsif index2 < len_of_a2
+			merged_array += array2[index2..-1]
+		end
+		merged_array												#Returns the merged array
+	end
+
+end
+
+
+
+# TEST CODE
 test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 test_array2 = [9,8,7,6,5,4,3,2,1,0]
 test_array3 = ['a', 'c', 'd', 'u', 'n', 'z', 'uganda', 'cow']
 
-tree = BinarySearchTree.new(test_array2)
+#Assign instance of class
+tree = BinarySearchTree.new(test_array)
+#Use the display_tree function for debug purposes
 tree.display_tree {|x| print "#{x} "}
 print "\n"
 
 p tree.depth_first_search(4)
+
+
+
+
